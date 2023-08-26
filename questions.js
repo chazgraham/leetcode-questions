b44efe961d0375191988f3c917f968f07620175f
chaz Graham <chazhg80.cg@gmail.com>
2023-08-25 22:41:21 -0500

    added problem and solution to check if an array contains duplicates

--- questions.js
+++ questions.js
@@ -1,4 +1,4 @@
-
+// checks for power of tree
 const isPowerOfThree = () => {
     let n = 22
     while(n > 1){
@@ -6,7 +6,17 @@ const isPowerOfThree = () => {
     }
     console.log(n)
     
-    return n === 1 ? console.log('true') : console.log('flase')
+    return n === 1 ? console.log('true') : console.log('false')
 };
 
 isPowerOfThree()
+
+// chescks to see if the array contains a duplicate
+
+const containsDuplicate = function () {
+    let nums = [1, 2, 4, 7, 8, 4]
+    const set = new Set(nums)
+    return set.size === nums.length ? console.log('false') : console.log('true')
+}
+
+containsDuplicate()
